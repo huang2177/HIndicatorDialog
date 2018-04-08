4caacc7f534136837821dbd093955ba41931e403
USER-20170711JQ\Administrator <15378412400@163.com>
2018-04-08 16:36:03 +0800

    1,提供了默认的适配器（HindcatorAdapter） 2,优化一些代码，增加一些功能

---
+++ mylibrary/src/main/java/com/huangbryant/hindicator/listener/OnItemClickListener.java
@@ -0,0 +1,14 @@
+package com.huangbryant.hindicator.listener;
+
+/**
+ * @author Huangshuang  2018/4/8 0008
+ */
+
+public interface OnItemClickListener {
+    /**
+     * 当Item被选中时调用
+     *
+     * @param position
+     */
+    void OnItemClick(int position);
+}
